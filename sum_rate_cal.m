0263d93ff7e8abce5cc2c186f5254188def0f259
Kangjian Chen <104707425+kjchen96@users.noreply.github.com>
2026-02-06 10:20:09 +0800

    Delete sum_rate_cal.m

--- sum_rate_cal.m
+++
@@ -1,8 +0,0 @@
-function rate  = sum_rate_cal(H,F,sigma2,K)
-         rate = 0;
-         for k = 1 : K
-             P_S = abs(H(:,k)' * F(:,k))^2;         % Signal Power
-             P_I = sum(abs(H(:,k)' * F).^2) - P_S;  % Noiser Power
-             rate = rate + log2(1 + P_S/(sigma2 + P_I));          % Sum-rate
-         end
-end
